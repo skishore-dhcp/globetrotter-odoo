900edb1f213d90e2510b8289f885f9252554ad43
skishore-dhcp <skish2024@gmail.com>
2026-01-03 14:10:37 +0530

    Initialize Supabase client in login.js

---
+++ login.js
@@ -0,0 +1,7 @@
+const supabaseUrl = "https://bspusiewpgusseviegpl.supabase.co";
+const supabaseKey = "sb_publishable_dSW3ZDoFoo0NYDlzULF-_Q_BKBte7W2";
+
+const supabase = window.supabase.createClient(
+  supabaseUrl,
+  supabaseKey
+);
